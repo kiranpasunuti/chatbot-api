ca62dfa1c1b8ee5cabf525dad9219b121e587945
saikiran <126646670+kiranpasunuti@users.noreply.github.com>
2025-02-22 15:35:14 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -5,6 +5,7 @@ import google.generativeai as genai
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
+import os
 genai.configure(api_key='AIzaSyC0uefxG-FBjr83Jj-RWGwSFUuvz_59gCk')
 generation_config = {
     "temperature": 1,
